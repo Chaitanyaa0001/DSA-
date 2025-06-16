c3738f1a172d466f89354ab8bbf7be6358b666b5
chaitanya <chaitanyakhurana0@gmail.com>
2025-06-16 13:24:27 +0530

    basic patterns comploetion

---
+++ patterns/NumberPatterns/CharPattern.java
@@ -0,0 +1,24 @@
+package patterns.NumberPatterns;
+
+import java.util.*;
+
+public class CharPattern {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char aplhabet  = 'A';
+
+        System.out.println("enter the number ");
+        int n = sc.nextInt();
+
+        for(int i = 1;i <=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(aplhabet);
+                aplhabet++;
+            }
+            System.out.println();
+
+        }
+    }
+}
