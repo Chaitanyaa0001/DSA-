4620ba035ba83592989aa70cb81b2cde5d0fc602
chaitanya <chaitanyakhurana0@gmail.com>
2025-06-21 14:48:15 +0530

    kadans algorithm

---
+++ kadanealgo/kadan.java
@@ -0,0 +1,26 @@
+package kadanealgo;
+
+public class kadan {
+
+    public static void kadanalosum(int numbers []){
+        int cs = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i<numbers.length;i++){
+                cs+= numbers[i];
+            if(cs < 0){
+                cs = 0;
+            }
+                                max =Math.max(cs, max);
+
+        }
+        System.out.println("kadan also sub arrayu sum " + max);
+    }
+
+    public static void main(String[] args) {
+        int numbers [] ={-2,-3,4,-1,-2,1,5,-3};
+        kadanalosum(numbers);
+
+        
+        
+    }
+}
