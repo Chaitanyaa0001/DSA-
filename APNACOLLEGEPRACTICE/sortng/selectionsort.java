24cd13833bdbfd85642c4b0b21ddf73b1b242a0f
chaitanya <chaitanyakhurana0@gmail.com>
2025-07-02 23:34:15 +0530

    changes

---
+++ APNACOLLEGEPRACTICE/sortng/selectionsort.java
@@ -0,0 +1,17 @@
+package sortng;
+
+public class selectionsort {
+    public static void selectionsort( int  [] arr ){
+        for(int i = 0;i < arr.length-2;i++){
+            for(int j = i+1; j< arr.length-1;j++){
+                System.out.println("ewbdfu3b");
+               
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr [] = {5,4,7,8,4,5,2,1};
+
+
+    }
+}
