3b024723018055cb781e99d16fc2d6ceb248e9f1
chaitanya <146080655+Chaitanyaa0001@users.noreply.github.com>
2025-07-28 22:02:11 +0530

    Update forloop.java

--- APNACOLLEGEPRACTICE/loops/ForLoops/forloop.java
+++ APNACOLLEGEPRACTICE/loops/ForLoops/forloop.java
@@ -5,7 +5,7 @@ public class forloop {
 
         for (int i = 1;i <=4;i++){
             for(int j =1;j<=4;j++){
-                System.out.print(" * ");
+                System.out.print(" **** ");
             }
             System.out.println(" ");
         }
