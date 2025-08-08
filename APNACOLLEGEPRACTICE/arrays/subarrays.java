8af09d1b8c430cfc59049636b2b2c3ed52b72367
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-08 18:20:23 +0530

    subarray

--- APNACOLLEGEPRACTICE/arrays/subarrays.java
+++ APNACOLLEGEPRACTICE/arrays/subarrays.java
@@ -4,21 +4,21 @@ public class subarrays {
 
     public static void findSubarrays(int[] numbers) {
         int total = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length; j++) {
-                // Print subarray from index i to j
-                for (int k = i; k <= j; k++) {
+        for(int i = 0; i< numbers.length; i++){
+            int start = i;
+            for(int j = i; j< numbers.length; j++){
+                int end = j;
+                for(int k = start; k <= end; k++){
                     System.out.print(numbers[k] + " ");
                 }
-                total++;
-                System.out.println();
+                System.out.println("");
             }
+            System.out.println(" ");
         }
-        System.out.println("Total subarrays = " + total);
     }
 
     public static void main(String[] args) {
-        int[] numbers = {2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numbers = {2, 4, 6, 8, 10};
         findSubarrays(numbers);
     }
 }
