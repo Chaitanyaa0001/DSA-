003c98e26c15b03f2d3d99cecc68c6a30ec1f47d
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-16 00:56:11 +0530

    profit loss

---
+++ LEETCODE/arrays/profitloss.java
@@ -0,0 +1,23 @@
+package LEETCODE.arrays;
+
+public class profitloss {
+    public static int maxprofit(int prices[]){
+        int buyingprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(buyingprice < prices[i]){
+                int profit = prices[i] - buyingprice;
+                maxprofit = Math.max(profit, maxprofit);
+            }else{
+                buyingprice = prices[i];
+            };
+        };
+        return  maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[] =  {7, 1, 5, 3, 6, 4} ;
+        int resut =   maxprofit(prices);
+        System.out.println(resut);
+       
+    }
+}
---
+++ important/trapingwater.java
@@ -0,0 +1,12 @@
+package important;
+
+public class trapingwater {
+
+    public static void water(int numbers[]){
+       
+    }
+    public static void main(String[] args) {
+        int numbers [] = {4,2,0,6,3,2,5};
+
+    }
+}
