e76caeeb4ba2349a41664c368f85b63a6c44f13a
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-20 22:53:53 +0530

    search in spiral matrix

---
+++ APNACOLLEGEPRACTICE/A2Darrays/arrays.java
@@ -0,0 +1,30 @@
+package A2Darrays;
+
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        // number of rows 
+        int n = matrix.length;
+        // number of columns 
+        int m = matrix[0].length;
+        // now input  
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i< n; i++ ){
+            for(int j = 0; j < m; j ++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+        // output 
+        for(int i = 0; i< n; i++ ){
+            for(int j = 0; j < m; j ++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
---
+++ APNACOLLEGEPRACTICE/A2Darrays/search.java
@@ -0,0 +1,42 @@
+package A2Darrays;
+
+import java.util.Scanner;
+
+public class search {
+
+    public static boolean search(int matrix[][], int key){
+        for(int i = 0; i< matrix.length; i++ ){
+            for(int j = 0; j < matrix[0].length; j ++){
+                if(matrix[i][j] == key){
+                    System.out.println("key found " + "(" + i+ "," + j + ")" );
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+        public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length;
+        
+        int m = matrix[0].length;
+        int key = 6;
+  
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i< n; i++ ){
+            for(int j = 0; j < m; j ++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+ 
+        for(int i = 0; i< n; i++ ){
+            for(int j = 0; j < m; j ++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        search(matrix, key);
+        
+    }
+}
