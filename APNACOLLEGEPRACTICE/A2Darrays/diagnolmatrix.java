c9affde8e709ae428bac8be1de982dbc7a3024e2
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-22 22:26:01 +0530

    push

--- APNACOLLEGEPRACTICE/A2Darrays/diagnolmatrix.java
+++ APNACOLLEGEPRACTICE/A2Darrays/diagnolmatrix.java
@@ -28,6 +28,7 @@ public class diagnolmatrix {
             }
         }
         return sum;
+        
     }
     public static void main(String[] args) {
         int matrix [][] = {{1,2,3},{4,5,6},{7,8,9}};
