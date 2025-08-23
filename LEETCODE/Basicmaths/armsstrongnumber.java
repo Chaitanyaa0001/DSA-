cd7ad5da06d669d7f8c6ae427b11417f3a6d514a
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-23 23:30:34 +0530

    push

--- LEETCODE/Basicmaths/armsstrongnumber.java
+++ LEETCODE/Basicmaths/armsstrongnumber.java
@@ -18,10 +18,8 @@ public class armsstrongnumber {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("enter the value of n ");
         int n = sc.nextInt();
-
         boolean result = isArmstriong(n);
         System.out.println(result);
     }
