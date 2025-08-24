0c0689b0a12da487d748f39ef191ddd6b8afe650
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-25 03:48:53 +0530

    leftroatate array by once

---
+++ LEETCODE/arrays/leftroatearray.java
@@ -0,0 +1,21 @@
+package LEETCODE.arrays;
+
+public class leftroatearray {
+    public static void leftRotate(int nums[]){
+        int temp = nums[0];
+        int n = nums.length;
+        for(int i = 1; i < n; i++ ){
+            nums[i-1] = nums[i];
+        }
+        nums[n-1] = temp;
+        for(int i = 0; i< n; i++){
+            System.out.print(nums[i] + "");
+        }
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4,5};
+        leftRotate(nums);
+    }
+}
