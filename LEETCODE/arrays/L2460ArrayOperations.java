b190d99d52b6832a4968cf597e71f8b1698ffc23
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-27 22:51:38 +0530

    array operations

--- LEETCODE/arrays/L2460ArrayOperations.java
+++ LEETCODE/arrays/L2460ArrayOperations.java
@@ -28,7 +28,21 @@ package LEETCODE.arrays;
 
 public class L2460ArrayOperations {
     public static int[] operationonarray(int nums[]){
-
+        int index = 0;
+        for(int i = 0; i < nums.length-1;i++){
+            if(nums[i]  == nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                index++;
+            }
+        }
         
         return nums;
     }
