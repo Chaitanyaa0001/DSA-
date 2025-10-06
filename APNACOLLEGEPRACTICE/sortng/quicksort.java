b1fce0151d26b1b22fd31534f75adfce275c1491
chaitanya <chaitanyakhurana0@gmail.com>
2025-10-07 00:20:06 +0530

    git push

--- APNACOLLEGEPRACTICE/sortng/quicksort.java
+++ APNACOLLEGEPRACTICE/sortng/quicksort.java
@@ -18,9 +18,6 @@ public class quicksort {
     //         arr[index+1] = arr[end];
     //         arr[end] = temp; 
     //     return index+1;
-
-
-    
     // }
     
     // public static void QuickSortLast(int arr[], int start, int end){
