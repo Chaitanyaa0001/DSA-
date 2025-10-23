6837258c507aca17db67d81aab008a438f56530c
chaitanya <chaitanyakhurana0@gmail.com>
2025-10-23 23:37:51 +0530

    push

--- APNACOLLEGEPRACTICE/A2Darrays/number7s.java
+++ APNACOLLEGEPRACTICE/A2Darrays/number7s.java
@@ -15,6 +15,10 @@ public class number7s {
         }
         return num;
         
+
+
+
+        
     }
     public static void main(String[] args) {
         int[][] array = { {4,7,8},{8,8,7} };
