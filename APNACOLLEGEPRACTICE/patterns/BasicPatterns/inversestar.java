6005433aeab8d80920b16c1fa7aa0a5034a08517
chaitanya <chaitanyakhurana0@gmail.com>
2025-11-10 00:42:57 +0530

    push

--- APNACOLLEGEPRACTICE/patterns/BasicPatterns/inversestar.java
+++ APNACOLLEGEPRACTICE/patterns/BasicPatterns/inversestar.java
@@ -9,8 +9,8 @@ public class inversestar {
         int n = sc.nextInt();
 
 
-        for(int i =1; i <=n;i++){
-            for (int j = 1;j <= n-i+1;j++){
+        for(int i =n; i >=1;i--){
+            for (int j = i;j >= 1;j--){
                 System.out.print("*");
             }
             System.out.println();
