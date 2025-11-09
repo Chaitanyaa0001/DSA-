1622b807c201fece0111f4fa5d0b1e9e5d957889
chaitanya <chaitanyakhurana0@gmail.com>
2025-11-10 00:56:41 +0530

    push

--- APNACOLLEGEPRACTICE/patterns/BasicPatterns/halfpyramid.java
+++ APNACOLLEGEPRACTICE/patterns/BasicPatterns/halfpyramid.java
@@ -6,7 +6,7 @@ public class halfpyramid {
     public static void main(String[] args) {
         Scanner Sc = new  Scanner(System.in);
         int n = Sc.nextInt();
-
+        
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print(" "+ j);
