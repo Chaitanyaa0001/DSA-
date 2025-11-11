deda886c5cdce240ca55e08e49ee731eb3796348
chaitanya <chaitanyakhurana0@gmail.com>
2025-11-12 02:01:48 +0530

    factorial

--- APNACOLLEGEPRACTICE/Functions/basicques/factorial.java
+++ APNACOLLEGEPRACTICE/Functions/basicques/factorial.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 public class factorial {
 
-    public static long factorial (long n){
-        long f =1;
+    public static int factorial(int n ){
 
-        for (int i =1;i<=n;i++){
-            f = f*i;
-        }
+        int f = 1;
+        for(int i=1;i <=n; i++){
+            f = f * i;
+        } 
+
         return f;
     }
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter tge value of n:");
-        long n = sc.nextInt();
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+
+       int susu =  factorial(n);
+       System.out.println(susu);
          
-        factorial(n);
-        System.out.println(factorial(n));
-        
     }
     
 }
