707f0d756f02c2949a1ab752fe076f327e1c946d
chaitanya <chaitanyakhurana0@gmail.com>
2025-11-17 23:59:12 +0530

    git push

--- APNACOLLEGEPRACTICE/Functions/practicequestions/sumofintegers.java
+++ APNACOLLEGEPRACTICE/Functions/practicequestions/sumofintegers.java
@@ -13,10 +13,6 @@ public class sumofintegers {
         };
         n = n/10;
         return sum;
-
-
-
-        
     };
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
