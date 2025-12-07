b2953b6806988aee3e4028e60a6096fba03dfe0c
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-08 00:13:40 +0530

    git push

--- LEETCODE/Basicmaths/S4_ArmsstrongNumber.java
+++ LEETCODE/Basicmaths/S4_ArmsstrongNumber.java
@@ -2,18 +2,34 @@ package LEETCODE.Basicmaths;
 
 import java.util.Scanner;
 
+// in armstrong number if we take 
+//  321 then the cube of the 3 , 2 and 1  should be equal to the 321 then it will be armstrong number 
+
+
 public class S4_ArmsstrongNumber {
     public static boolean isArmstriong(int n){
-        int power = (int)Math.log10(n) + 1;
-        int num = n;
-        int armnumber = 0;
-        while (num>0) {
-            int lastdigit = num % 10;
-            armnumber = armnumber  + (int)Math.pow(lastdigit,power);
-            num /=10;
+        int number = 0;
+        int pow = (int) Math.log10(n) +1;
+        int original = n;
+        // if number if -ve 
+        if(n < 0 ){
+            return false;
         }
-        return armnumber == n ;
- 
+        // if number is 0 as 0 is armstrong number 
+        if(n == 0){
+            return true;
+        }
+        // to check weather number is armstrong or not 
+        while (n > 0) {
+            int digit = n %10;
+            number = number + (int) Math.pow(digit, pow);
+            n = n /10;
+        }
+        if(original == number){
+            return true;
+        }else{
+            return false;
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
