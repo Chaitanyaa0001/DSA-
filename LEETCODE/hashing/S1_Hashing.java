9f5616ffd14697ad71991cf5bf09e5df01d230d6
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-10 03:02:08 +0530

    hashing

--- LEETCODE/hashing/S1_Hashing.java
+++ LEETCODE/hashing/S1_Hashing.java
@@ -1,38 +1,40 @@
-package LEETCODE.hashing;
+    package LEETCODE.hashing;
 
+    import java.util.*;
 
-import java.util.*;
+    public class S1_Hashing {
 
-public class S1_Hashing {
+        public static void calculateFrequency(int[] arr, int q){
+            int count = 0;
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] == q){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+        
 
-    public static void calculateFrequency(int[] arr, int q){
-        int count = 0;
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == q){
-                count++;
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            // Read array (no messages)
+            for(int i = 0; i < arr.length; i++){
+                arr[i] = sc.nextInt();
+            }
+
+            // pre computation 
+            int[] hasharr = new int[1000000000];
+            for(int i = 0; i <arr.length; i++){
+                hasharr[arr[i]]++;
+            }
+            int q = sc.nextInt();
+            for(int i = 0; i < q; i++){
+                int x = sc.nextInt();
+
+                System.out.println( hasharr[x]);
             }
         }
-        System.out.println(count);
     }
-
-    
-
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        // Read array (no messages)
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
-        }
-        // Number of queries
-        int q = sc.nextInt();
-        // For each query read a number and print its frequency
-        for(int i = 0; i < q; i++){
-            int x = sc.nextInt();
-            calculateFrequency(arr, x);
-        }
-    }
-}
