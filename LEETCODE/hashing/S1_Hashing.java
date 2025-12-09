8e6f24a5b0e5c71032297027dcba8bdac40cfcfa
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-09 23:45:58 +0530

    git push

--- LEETCODE/hashing/S1_Hashing.java
+++ LEETCODE/hashing/S1_Hashing.java
@@ -13,24 +13,22 @@ public class S1_Hashing {
                 count++;
             }
         }
-
         System.out.println(count);
     }
+
+    
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int[] arr = new int[n];
-
         // Read array (no messages)
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-
         // Number of queries
         int q = sc.nextInt();
-
         // For each query read a number and print its frequency
         for(int i = 0; i < q; i++){
             int x = sc.nextInt();
