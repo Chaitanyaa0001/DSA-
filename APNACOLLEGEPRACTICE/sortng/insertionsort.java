61fc9038b5499729568a790b35539d6004960d15
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-11 02:59:40 +0530

    git push

--- APNACOLLEGEPRACTICE/sortng/insertionsort.java
+++ APNACOLLEGEPRACTICE/sortng/insertionsort.java
@@ -1,23 +1,19 @@
 package sortng;
 
 public class insertionsort {
-    public static void insertionSort(int arr[]){
-        for(int i = 1; i < arr.length; i++){
-            // to find and to temporary store the current elemnt so that it can be inserted 
-            int currentelement = arr[i]; 
-            // this is the previous elemnt thorugh which we can do backtract to comare my elemnets 
-            int previous = i-1;
-            // to find the element 
-            // loop when our previos elemnt is greater than 0 and greater than the current element 
-            while (previous >= 0 && arr[previous] > currentelement) {
-                // then find the elemnt form backtrack 
-                arr[previous +1] = arr[previous];
-                previous --;
+
+    public static void insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 1; i< n; i++){
+            while (i > 0 && arr[i-1] > arr[i] ) {
+                    int temp = arr[i-1];
+                    arr[i-1] = arr[i];
+                    arr[i] = temp;
+                    i--;
             }
-            // to insertion 
-            arr[previous + 1] = currentelement;
         }
     }
+
 
     public static void printInsertionSort(int arr[]){
         for(int i = 0; i< arr.length; i++){
@@ -27,8 +23,11 @@ public class insertionsort {
     }
     public static void main(String[] args) {
         int arr [] = {4,2,6,1,5,3};
-
+        // {2,4,6,1,5,3} i=1; prev = 0
+        // {2,4,6,1,5,3 } prev =1 i = 2
+        // {2,4,1,6,5,3 } prev = 2 i = 3
         insertionSort(arr);
+
         printInsertionSort(arr);
     }
 }
