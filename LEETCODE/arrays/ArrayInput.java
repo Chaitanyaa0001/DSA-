625575f1dd16a1bc1f2b67d2efda92ae48090604
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-11 04:58:39 +0530

    git push

---
+++ LEETCODE/arrays/ArrayInput.java
@@ -0,0 +1,20 @@
+package LEETCODE.arrays;
+
+import java.util.Scanner;
+
+public class ArrayInput {
+
+    public static int[] takeArrayInput() {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        // System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        return arr;
+    }
+}
--- LEETCODE/arrays/Easy/S1_Largestelement.java
+++ LEETCODE/arrays/Easy/S1_Largestelement.java
@@ -1,12 +1,11 @@
 
 import java.util.Scanner;
-
+import LEETCODE.arrays.ArrayInput;
 public class S1_Largestelement {
 
     public static int findlargest(int[] arr){
         int largestValue = Integer.MIN_VALUE;
         int n = arr.length;
-
         for(int i = 0; i < n; i++){
             if(arr[i]> largestValue){
                 largestValue = arr[i];
@@ -15,14 +14,7 @@ public class S1_Largestelement {
         return largestValue;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n  = sc.nextInt();
-        int[] arr = new int[n];
-        
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
-        }
-
+       int [] arr = ArrayInput.takeArrayInput();
        int result =  findlargest(arr);
        System.out.println(result);
     }
