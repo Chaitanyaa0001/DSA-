de17fb68b361c51473d5910a604f2de9a6481fa0
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-15 22:03:49 +0530

    git push

---
+++ LEETCODE/arrays/Easy/blindQues/Leetcode_1920_Buildarrayfrompermutation.java
@@ -0,0 +1,41 @@
+package blindQues;
+
+import LEETCODE.arrays.ArrayInput;
+
+public class Leetcode_1920_Buildarrayfrompermutation {
+    public static int[] functionnntocalculatearray(int nums[]){
+        // brute force 
+        int n = nums.length;
+        int[] temp = new int[n];
+        int x = 0;
+        for(int i = 0; i < n; i++){
+            temp[x++] =  nums[nums[i]];
+        }
+        return temp;
+    }
+
+    // optimal solution 
+    public static int[] optimalarrproch(int [] nums){
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            nums[i] = nums[i] + (nums[nums[i]] % n) * n;
+        }
+        for(int i = 0; i < n; i++){
+            nums[i] = nums[i]/ n;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int nums[]  = ArrayInput.takeArrayInput();
+
+        int [] result = functionnntocalculatearray(nums); 
+                int [] result2 = optimalarrproch(nums);
+
+
+        for(int i = 0; i < result2.length; i++){
+            System.out.print(result[i]);
+        }
+
+    }
+}
