4f6380e058c7b2de36d177dbab15ecf169be43fb
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-19 23:56:12 +0530

    git push

--- LEETCODE/arrays/Easy/L485MaxConsicutiveOnes.java
+++ LEETCODE/arrays/Easy/L485MaxConsicutiveOnes.java
@@ -1,7 +1,5 @@
 // Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
- 
-
 // Example 1:
 
 // Input: nums = [1,1,0,1,1,1]
@@ -11,13 +9,10 @@
 
 // Input: nums = [1,0,1,1,0,1]
 // Output: 2
- 
 
 // Constraints:
-
 // 1 <= nums.length <= 105
 // nums[i] is either 0 or 1.
-
 
 public class L485MaxConsicutiveOnes {
     public static int findmaxconsecutiveones(int nums[]) {
