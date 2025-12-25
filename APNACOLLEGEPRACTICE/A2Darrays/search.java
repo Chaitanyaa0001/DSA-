8d415cc4165def959bb9abaa4d94af99538a68f7
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-25 22:19:27 +0530

    git push

--- APNACOLLEGEPRACTICE/A2Darrays/search.java
+++ APNACOLLEGEPRACTICE/A2Darrays/search.java
@@ -19,7 +19,6 @@ public class search {
         public static void main(String[] args) {
         int matrix[][] = new int[3][3];
         int n = matrix.length;
-        
         int m = matrix[0].length;
         int key = 6;
         
