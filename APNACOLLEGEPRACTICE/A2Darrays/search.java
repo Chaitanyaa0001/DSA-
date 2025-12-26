57867eef9a2b317bf15a4251c2b69a91888d2a0d
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-26 23:43:40 +0530

    git push

--- APNACOLLEGEPRACTICE/A2Darrays/search.java
+++ APNACOLLEGEPRACTICE/A2Darrays/search.java
@@ -29,14 +29,12 @@ public class search {
                 matrix[i][j] = sc.nextInt();
             }
         }
- 
         for(int i = 0; i< n; i++ ){
             for(int j = 0; j < m; j ++){
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-
         searchm(matrix, key);
         
     }
