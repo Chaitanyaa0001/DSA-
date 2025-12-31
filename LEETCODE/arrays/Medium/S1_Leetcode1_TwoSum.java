60f852478d9399d4ae89fe51965355eaa5bc4a34
chaitanya <chaitanyakhurana0@gmail.com>
2025-12-31 22:33:41 +0530

    git push

--- LEETCODE/arrays/Medium/S1_Leetcode1_TwoSum.java
+++ LEETCODE/arrays/Medium/S1_Leetcode1_TwoSum.java
@@ -1,5 +1,7 @@
 package LEETCODE.arrays.Medium;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import LEETCODE.arrays.ArrayInput;
@@ -7,13 +9,27 @@ import LEETCODE.arrays.ArrayInput;
 public class S1_Leetcode1_TwoSum {
     // brute force 
     public static int[] bruteforce(int nums[], int target){
-
+        int n = nums.length;
+        for(int i = 0; i < n; i++ ){
+            for(int j = 0; j < n; j++){
+                if(nums[i] + nums[j] == target){
+                    return new  int[] {i,j};
+                }
+            }
+        }
         return new int [] {};
     }
     
     // optiml 
     public static int[] optimalApproch(int nums[], int target){
-
+        int n = nums.length;
+        HashMap<Integer,Integer> hm =  new HashMap<>();
+        for(int i = 0 ; i < n; i ++){
+            if(hm.containsKey(target - nums[i])){
+                return new int[] {i, hm.get(target-nums[i])};
+            }
+            hm.put(nums[i], i);
+        }
         return new int [] {};
     }
 
@@ -24,10 +40,9 @@ public class S1_Leetcode1_TwoSum {
         int target  = sc.nextInt();
         // brute 
         int[] brute = bruteforce(arr, target);
-        System.out.println(brute);
-        // optimal 
-        int[] optimal =  optimalApproch(brute, target);
-        System.out.println(optimal);
-
+        System.out.println(Arrays.toString(brute));
+        
+        int[] optimal = optimalApproch(arr, target);
+        System.out.println(Arrays.toString(optimal));
     }
 }
