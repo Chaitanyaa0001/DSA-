cd6e2a7031ef06a3ae79dff2f39aee72ecb6868b
chaitanya <chaitanyakhurana0@gmail.com>
2026-01-08 00:04:24 +0530

    git push

--- LEETCODE/arrays/Medium/S10_Leetcode73_2DMatrixSetTozero.java
+++ LEETCODE/arrays/Medium/S10_Leetcode73_2DMatrixSetTozero.java
@@ -55,9 +55,6 @@ public class S10_Leetcode73_2DMatrixSetTozero {
         }
         
     }
-
-
-
     // now better  solution 
     public static void  betterapproch(int  matrix[][]){
         int n = matrix.length;
@@ -80,10 +77,46 @@ public class S10_Leetcode73_2DMatrixSetTozero {
             }
         }
     }
-
-
-
     // best approch 
+
+    public static void bestapproch(int matrix[][]){
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int col0 = 1;
+        // row matrix[0][...] 
+        // col matrix[...][0]
+        for(int i = 0; i < n; i++){
+            if(matrix[i][0] == 0){
+                col0 = 0;
+            }
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][i] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][i] == 0 ||matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
     
     public static void main(String[] args) {
         int matrix[][] = {{1,1,1},{1,0,1},{1,1,1}};
