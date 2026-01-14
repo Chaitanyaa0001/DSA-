ecc49f8265596130bfbf81ad635baf72be38c9f2
chaitanya <chaitanyakhurana0@gmail.com>
2026-01-14 15:11:02 +0530

    git push

---
+++ LEETCODE/arrays/Hard/S11_CountInversions.java
@@ -0,0 +1,9 @@
+package LEETCODE.arrays.Hard;
+
+import LEETCODE.arrays.ArrayInput;
+
+public class S11_CountInversions {
+    public static void main(String[] args) {
+        int[] nums = ArrayInput.takeArrayInput();
+    }
+}
