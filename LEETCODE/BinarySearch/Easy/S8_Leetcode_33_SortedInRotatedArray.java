152a000f671c26c2ea996a4139c9e283ba3b7a30
chaitanya <chaitanyakhurana0@gmail.com>
2026-01-24 23:13:10 +0530

    git push

---
+++ LEETCODE/BinarySearch/Easy/S8_Leetcode_33_SortedInRotatedArray.java
@@ -0,0 +1,16 @@
+package LEETCODE.BinarySearch.Easy;
+
+import LEETCODE.BinarySearch.ArrayInp;
+
+public class S8_Leetcode_33_SortedInRotatedArray {
+    public static int seacth(int[] nums, int target){
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        // nums = [4,5,6,7,0,1,2], target = 0
+        //  nums = [4,5,6,7,0,1,2], target = 3
+        int [] nums = ArrayInp.takeArrayInput();
+        seacth(nums, 0);
+    }
+}
