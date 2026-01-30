4cb2429de44eb634a65677112179d2c3f01205f9
chaitanya <chaitanyakhurana0@gmail.com>
2026-01-30 23:51:48 +0530

    git binary search

---
+++ LEETCODE/BinarySearch/Medium/S4_Leetcode_1283_FIndThesmallestDivisior.java
@@ -0,0 +1,41 @@
+package LEETCODE.BinarySearch.Medium;
+
+import java.util.Scanner;
+
+import LEETCODE.BinarySearch.ArrayInp;
+
+public class S4_Leetcode_1283_FIndThesmallestDivisior {
+    public static int isDivisor(int [] arr, int divisor){
+        int n = arr.length;
+        int totalDivSum = 0;
+        for(int i = 0; i < n; i++){
+            totalDivSum +=(int) Math.ceil((double)arr[i] /  divisor);
+        }
+        return totalDivSum;
+    }
+
+
+    public int smallestDivisor(int[] nums, int threshold) {
+        // range -> arr max
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max,nums[i]);
+        } 
+        int start = 1, end = max;
+        while(end >= start){
+            int mid = start + (end - start) / 2;
+            if(isDivisor(nums,mid) <= threshold){
+                end = mid -1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        int [] nums = ArrayInp.takeArrayInput();
+        Scanner sc = new Scanner(System.in);
+        int threshold = sc.nextInt();
+    }
+}
---
+++ LEETCODE/BinarySearch/Medium/S5_leetcode_1011_CapacityToShipPacakages.java
@@ -0,0 +1,20 @@
+package LEETCODE.BinarySearch.Medium;
+
+import java.util.Scanner;
+
+import LEETCODE.BinarySearch.ArrayInp;
+
+public class S5_leetcode_1011_CapacityToShipPacakages {
+    public static int checkdays(int[] arr, int capacity){
+
+    }
+    public static int brute_force(int[] weights, int days){
+        
+    }
+
+    public static void main(String[] args) {
+        int weights[] = ArrayInp.takeArrayInput();
+        Scanner sc = new Scanner(System.in);
+        int days = sc.nextInt();
+    }
+}
