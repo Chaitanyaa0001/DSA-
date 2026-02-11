0af1c9a11ee48e681e551ead9f80589e9bf6fb74
chaitanya <chaitanyakhurana0@gmail.com>
2026-02-11 14:32:53 +0530

    git push

---
+++ LEETCODE/BinarySearch/2DArrays/S5_FindMedianIn2DMartix.java
@@ -0,0 +1,7 @@
+public class S5_FindMedianIn2DMartix {
+
+    public static void main(String[] args) {
+        int [][] matrix = {{1,2,3,4,5,6,7},{1,2,3,4,5,6},{2,4,5,6,7,8,9},{1,2,3,4,5,6,7,8}};
+        
+    }
+}
