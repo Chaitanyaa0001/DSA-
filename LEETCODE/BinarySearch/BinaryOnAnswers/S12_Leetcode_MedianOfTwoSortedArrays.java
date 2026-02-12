f0d97f1787d94f82ba76f79c516efe11972d6a4b
chaitanya <chaitanyakhurana0@gmail.com>
2026-02-13 02:45:48 +0530

    git push

--- LEETCODE/BinarySearch/BinaryOnAnswers/S12_Leetcode_MedianOfTwoSortedArrays.java
+++ LEETCODE/BinarySearch/BinaryOnAnswers/S12_Leetcode_MedianOfTwoSortedArrays.java
@@ -35,6 +35,8 @@ public class S12_Leetcode_MedianOfTwoSortedArrays {
         if( k % 2 == 1){
             return list.get(k/2);
         }
+
+        return -1;
     }
 
     public static int betterApproch(int[] nums1, int[] nums2){
@@ -87,11 +89,48 @@ public class S12_Leetcode_MedianOfTwoSortedArrays {
     }
 
     public static int  binary_Approch(int[] nums1, int[] nums2){
-        
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        // we have to take the short array so that we can reduce the time complexity 
+        if ( n1 > n2) return binary_Approch(nums2, nums1);
+        // total array length 
+        int n = n1 + n2;
+        // this is how many elements should be there on the left side of the array 
+        int left = (n1 + n2 + 1) / 2;
+        int  start = 0, end = n1 ;
+
+        while (start <= end) {
+            int mid1 = start + (end - start) / 2;
+            int mid2 = left - mid1;
+            // if value is not there then we in left we have to take the max element 
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+        //    if value is not there on right then we have to take the min element  
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            // check that our all elements l1,l2,r1,r2 lies between the boundries 
+            if( mid1 < n1) r1 = nums1[mid1]; //  r1 should be in the range of array 1 of right side  r1 < n1
+            if( mid2 < n2) r2 = nums2[mid2]; // r2 should be in the range of array 2 of right side r2 < n2
+            if(mid1 -1 >= 0) l1 = nums1[mid1-1]; // l1 should exist in the range it should not end up at -1 hypothetical index l1 >= 0
+            if(mid2 -1 > 0) l2 = nums2[mid2 -1]; // l1 should exist in the range it should not end up at -1 hypothetical index l1 >= 0
+
+            // if our this condition meets then our array is this 
+            if(l1 < r2 && l2 < r1){
+                // check for if our array is odd then we need to find the max of l1 and l2 beacuse it will be our median 
+                if(n % 2 == 1){
+                    return Math.max(l1, l2);
+                } else{
+                    // for odd we will take max elenet from left side and min element from right side  and take  median 
+                  return  (Math.max(l1,l2) + Math.min(r1, r2)) / 2;
+                }    
+            }else if( l2 > r1){
+                start = mid1 + 1;
+            }else{
+                end = mid1 - 1;
+            }
+        }
         return -1;
     }
 
-    
+
     public static void main(String[] args) {
         int [] nums = ArrayInp.takeArrayInput();
         
