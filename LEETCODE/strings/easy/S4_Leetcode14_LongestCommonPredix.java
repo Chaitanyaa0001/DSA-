a2859cbae3090206a8cd4bbb0ba57105db6d6890
chaitanya <chaitanyakhurana0@gmail.com>
2026-02-21 23:54:06 +0530

    git push

--- LEETCODE/strings/easy/S4_Leetcode14_LongestCommonPredix.java
+++ LEETCODE/strings/easy/S4_Leetcode14_LongestCommonPredix.java
@@ -1,5 +1,10 @@
 package LEETCODE.strings.easy;
 
+import java.util.Scanner;
+
 public class S4_Leetcode14_LongestCommonPredix {
-    
+    public static void main(String[] args) {
+        Scanner SC  = new Scanner(System.in);
+        String strs[] = {"flower","flow","flight"};
+    }
 }
