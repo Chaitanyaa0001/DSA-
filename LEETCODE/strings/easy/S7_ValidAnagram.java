8f0d54e972a3b11a0f905283d3c948c517a8a071
chaitanya <chaitanyakhurana0@gmail.com>
2026-02-23 01:46:44 +0530

    git push

--- LEETCODE/strings/easy/S7_ValidAnagram.java
+++ LEETCODE/strings/easy/S7_ValidAnagram.java
@@ -1,5 +1,24 @@
 package LEETCODE.strings.easy;
 
+import java.util.Scanner;
+
 public class S7_ValidAnagram {
-    
+    public static Boolean validanagram(String s, String t){
+        int[] arr = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for(int i : arr){
+            if(i != 0){
+                return false ;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s  = sc.nextLine();
+        String t = sc.nextLine();
+    }
 }
--- LEETCODE/strings/medium/S1_Leetcode_CountFreqofCharAndArrangeInDesOrder.java
+++ LEETCODE/strings/medium/S1_Leetcode_CountFreqofCharAndArrangeInDesOrder.java
@@ -6,41 +6,7 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class S1_Leetcode_CountFreqofCharAndArrangeInDesOrder {
-    class Pair{
-        char character;
-        int freq;
-        Pair(char character, int freq){
-            this.character = character;
-            this.freq = freq;
-        }
-    }
-    public String frequencySort(String s) {
-        // ek map create kiya usme char or uski freq store 
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char i : s.toCharArray()){
-            // put,get TC : o(1)
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        // max heap priority queue jo freq ko decending mei store karega 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> b.freq - a.freq);
-        for(Map.Entry<Character,Integer> entry : map.entrySet()){
-            // TC: o(klog k) 
-            pq.add(new Pair(entry.getKey(), entry.getValue()));
-        }
-        // ab string build karo 
-        StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            // poll Tc : log k
-            Pair p = pq.poll();
-            // o(n)
-            for(int i = 0; i < p.freq; i++){
-                
-                
-                sb.append(p.character);
-            }
-        }
-        return sb.toString();
-    }
+   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
