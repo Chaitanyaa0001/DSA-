599dd921238824fc1a0f4901b5a0a6c87c514fa5
chaitanya <chaitanyakhurana0@gmail.com>
2026-03-06 00:32:18 +0530

    git push

---
+++ LEETCODE/LinkedList/mediumquesofLL/S11_Merge2SortedLinkedlist.java
@@ -0,0 +1,16 @@
+package LEETCODE.LinkedList.mediumquesofLL;
+
+import LEETCODE.LinkedList.LinkedListUtil;
+import LEETCODE.LinkedList.LinkedListUtil.Node;
+
+public class S11_Merge2SortedLinkedlist {
+    public static Node bruteforce(Node head){
+        
+        return head;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        Node head = LinkedListUtil.arrayToLinkedList(arr);
+
+    }
+}
