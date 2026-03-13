9ca21b07196faee80cbb2f2ebf8cb245cadf86b1
chaitanya <chaitanyakhurana0@gmail.com>
2026-03-13 22:36:32 +0530

    git push

---
+++ LEETCODE/LinkedList/hardofLL/S1_LeetcodeReverseNodeinKgroup.java
@@ -0,0 +1,17 @@
+package LEETCODE.LinkedList.hardofLL;
+
+import java.util.LinkedList;
+
+import LEETCODE.LinkedList.LinkedListUtil.Node;
+
+public class S1_LeetcodeReverseNodeinKgroup {
+    private static  Node approch(Node head){
+        Node temp = head;
+
+    }
+    public static void main(String[] args) {
+        LinkedList l = new LinkedList<>();
+        
+        insert(1);
+    }
+}
---
+++ LEETCODE/LinkedList/hardofLL/S3_Swap_Nodes_Leetcode.java
@@ -0,0 +1,47 @@
+package LEETCODE.LinkedList.hardofLL;
+
+import LEETCODE.LinkedList.LinkedListUtil.Node;
+
+public class S3_Swap_Nodes_Leetcode {
+
+    private  static Node  reverse(Node head){
+        Node temp = head;Node prev = null;
+        while(temp != null){
+            Node front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return temp;
+    }
+
+    public Node swapPairs(Node head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node t1 = head;
+        Node prev = null;
+        while(t1 != null){
+            Node t2 = t1.next;
+            if(t2 == null){
+                prev.next = t1;
+                break;
+            }
+            Node nextNode = t2.next;
+            t2.next = null;
+            reverse(t1);
+            if(t1 == head){
+                head = t2;
+            }else{
+                prev.next = t2;
+            }
+            prev = t1;
+            t1 = nextNode;
+        }
+        return head;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
