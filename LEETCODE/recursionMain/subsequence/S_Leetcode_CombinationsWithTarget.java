cf986a2fb1ce90f72ea43ffe35a81dbcd067c7ac
chaitanya <chaitanyakhurana0@gmail.com>
2026-03-21 00:22:20 +0530

    git [ush

--- LEETCODE/recursionMain/subsequence/S_Leetcode_CombinationsWithTarget.java
+++ LEETCODE/recursionMain/subsequence/S_Leetcode_CombinationsWithTarget.java
@@ -1,11 +1,30 @@
 package LEETCODE.recursionMain.subsequence;
 
+import java.util.*;;
 public class S_Leetcode_CombinationsWithTarget {
+    private  static  void helper(int i, int target,int arr[] ,List<Integer> list, List<List<Integer>> result){
+        if(i  ==  arr.length){
+            if(target == 0){
+                result.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        if(arr[i] <= target){
+            list.add(arr[i]);
+            helper(i, target - arr[i], arr, list, result);
+            list.remove(list.size() - 1);
+        }
+        helper(i+1, target, arr, list, result);
+    }
 
     private static List<List<Integer>> countcombination(int[] nums, int target){
+        List<List<Integer>> result = new ArrayList<>();
+        helper(0, target, nums, new ArrayList<>(), result);
+        return result;
 
     }
     public static void main(String[] args) {
-        
+        int [] nums = {2,3,6,7};
+        countcombination(nums, 7)   ;
     }
-}
+} 
