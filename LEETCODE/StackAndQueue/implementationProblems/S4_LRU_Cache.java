218b4c9f88badbcf25373310ad3f6d598e7cbe72
chaitanya <chaitanyakhurana0@gmail.com>
2026-05-07 23:27:57 +0530

    p

---
+++ LEETCODE/StackAndQueue/implementationProblems/S4_LRU_Cache.java
@@ -0,0 +1,27 @@
+package LEETCODE.StackAndQueue.implementationProblems;
+
+public class S4_LRU_Cache {
+
+    class LRUCache {
+
+        public LRUCache(int capacity) {
+
+        }
+
+        public int get(int key) {
+        
+        }
+
+        public void put(int key, int value) {
+        
+        }
+
+    }
+    public static void main(String[] args) {
+        // we neeed to initialize lru capacity to a positive interger capacity 
+        //  1- get function     return value of the key if key exist otherwise return 1 
+        // 2- Update the value of the key if the key exists. Otherwise, 
+        // add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation,
+        //  evict the least recently used key.
+    }
+}
