9f31264c399b4ce2cd5f390aba2e042b905b5ad8
chaitanya <chaitanyakhurana0@gmail.com>
2026-05-18 23:14:18 +0530

    p

--- LEETCODE/SlidingWindow/S9_BinaryArray.java
+++ LEETCODE/SlidingWindow/S9_BinaryArray.java
@@ -4,8 +4,21 @@ public class S9_BinaryArray {
     private static int bruteforce(int[] a){
         int maxlen = 0;
         int n  = a.length;
-        return maxlen;
+        int count  = 0;
+        for(int i = 0 ; i < n; i++){
+            int sum = 0;
+            for(int j = i ; j < n; j++){
+                if(sum <= 2){
+                   count++;
+                }else{
+                    break;
+                }
+            }
+        }
+        return count;
     }
+
+    private static int 
 
     public static void main(String[] args) {
         int[] a = {2,4,5,2,4,6};
