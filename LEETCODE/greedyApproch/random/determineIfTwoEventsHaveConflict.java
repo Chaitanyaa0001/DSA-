6ea53e9c6510c0b36e92f1bca37eb2ccd716e623
Chaitanya0001 <chaitanyakhurana0@gmail.com>
2026-06-08 22:48:35 +0530

    greedy

--- LEETCODE/greedyApproch/random/determineIfTwoEventsHaveConflict.java
+++ LEETCODE/greedyApproch/random/determineIfTwoEventsHaveConflict.java
@@ -1,10 +1,14 @@
 package LEETCODE.greedyApproch.random;
 
 public class determineIfTwoEventsHaveConflict {
-    private static int evintents(String event1[], String event2[]){
-        int count = 0; 
-        count++;
-        return count;
+    private static boolean evintents(String event1[], String event2[]){
+    String s1 = event1[0];
+    String e1 = event1[1];
+
+    String s2 = event2[0];
+    String e2 = event2[1];
+
+        return s1.compareTo(e2) <= 0 && s2.compareTo(e1) <= 0;
     }
     public static void main(String[] args) {
         String[] event1 = {"11:00","12:00"};
