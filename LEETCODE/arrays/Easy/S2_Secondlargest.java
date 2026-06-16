3b2581e20133e4c5348f1c99bda1a09b49ec6048
Chaitanyaa0001 <chaitanyakhurana0@gmail.com>
2026-06-17 00:26:34 +0530

    p

--- LEETCODE/arrays/Easy/S2_Secondlargest.java
+++ LEETCODE/arrays/Easy/S2_Secondlargest.java
@@ -11,9 +11,12 @@ public class S2_Secondlargest {
             if(numbers[i] > largest){
                 secondlargest = largest;
                 largest = numbers[i];
-
             }else if(numbers[i] > secondlargest && numbers[i] != largest){
-                // ab agar meri current vi value largest to hai nhi  to matlab vo choti hai usse  ab check karo agar current ki value meri secordlargest se badi hai or largest  mei jo store tha vo vlaue or meri current same nhi hona chaoye for duplicates 
+                // ab agar meri current vi value largest to hai nhi 
+                //  to matlab vo choti hai usse  ab check karo agar current 
+                // ki value meri secordlargest se badi hai or largest  mei
+                //  jo store tha vo vlaue or meri current same nhi hona 
+                // chaoye for duplicates 
                 secondlargest = numbers[i];
             }
         }
