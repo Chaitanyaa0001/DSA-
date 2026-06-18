44b2975f52007ced7a870bc0cb53536006db23aa
Chaitanyaa0001 <chaitanyakhurana0@gmail.com>
2026-06-18 19:22:54 +0530

    binary tree traversals

---
+++ LEETCODE/trees/traversalTechniques/BredthFirstSearch./LeverOrderTraversal.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import LEETCODE.trees.BinaryTree;
+
+
+public class LeverOrderTraversal {
+    // in this we wil queue data stricture 
+    private static List<List<Integer>> lot(BinaryTree.Node root){
+        List<List<Integer>> ans = new ArrayList<>();
+        return ans;
+    }
+    public static void main(String[] args) {
+        BinaryTree.Node root = BinaryTree.createTree();
+        lot(root);
+    }
+}
