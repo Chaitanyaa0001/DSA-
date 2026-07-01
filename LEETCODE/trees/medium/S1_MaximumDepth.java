640b03f75c2c2d34fdf449c024b6478a562e110e
Chaitanyaa0001 <chaitanyakhurana0@gmail.com>
2026-07-01 23:36:52 +0530

    trees

---
+++ LEETCODE/trees/medium/S1_MaximumDepth.java
@@ -0,0 +1,53 @@
+package LEETCODE.trees.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+import LEETCODE.trees.BinaryTree;
+
+public class S1_MaximumDepth {
+
+    
+    private static int findmaxdeptOfaNodeRECURSIVE(BinaryTree.Node root){
+        if(root == null){
+            return 0;
+        }
+        int left = findmaxdeptOfaNodeBFS(root.left);
+        int right = findmaxdeptOfaNodeBFS(root.right);
+        
+        return 1+ Math.max(left, right);
+    }
+    // --------------------------------------------------------
+
+    private static int findmaxdeptOfaNodeBFS(BinaryTree.Node root){
+       if(root == null){
+           return 0;
+       }
+       Queue<BinaryTree.Node> queue = new LinkedList<>();
+       int count = 0; 
+       queue.add(root);
+       
+       while(!queue.isEmpty()){
+           int size = queue.size();
+           for(int i = 0; i < size; i++){
+               BinaryTree.Node curr = queue.poll();
+               if(curr.left != null){
+                   queue.add(curr.left);
+               }
+               if(curr.right != null){
+                   queue.add(curr.right);
+               }
+           }
+           count++;
+       }
+       return count;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree.Node root = BinaryTree.createTree();
+        findmaxdeptOfaNodeBFS(root);
+        findmaxdeptOfaNodeRECURSIVE(root);
+    }
+}
---
+++ LEETCODE/trees/medium/S2_BalancedTree.java
@@ -0,0 +1,50 @@
+package LEETCODE.trees.medium;
+
+import LEETCODE.trees.BinaryTree;
+
+public class S2_BalancedTree {
+    private static int  getHeight(BinaryTree.Node root){
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return 1+ Math.max(left,right);
+    }
+    private static boolean bruteforce(BinaryTree.Node root){
+        if(root == null){
+            return true;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if(Math.abs(left - right) <= 1 && bruteforce(root.left) && bruteforce(root.right) ){
+            return true;
+        }
+        return false;
+    }
+
+    private static int optimalAppproch(BinaryTree.Node root){
+        
+        int left = optimalAppproch(root.left);
+        int right = optimalAppproch(root.right);
+
+        if(left == -1){
+            return -1;
+        }
+        if(right == -1){
+            return -1;
+        }
+        if(Math.abs(left - right) > 1){
+            return -1;
+        }   
+
+        return 1 + Math.max(left, right);
+    }
+
+    private static boolean ans(BinaryTree.Node root){
+        return optimalAppproch(root) != -1;
+    }
+    public static void main(String[] args) {
+        BinaryTree.Node  root = BinaryTree.createTree();
+        bruteforce(root);
+
+        ans(root);
+    }
+}
