4f6e9f10a0dab64bbfbe8c518540876442069f28
Chaitanyaa0001 <chaitanyakhurana0@gmail.com>
2026-07-16 23:59:44 +0530

    o

---
+++ LEETCODE/BinarySearchTrees/problems/SearchInBST.java
@@ -0,0 +1,11 @@
+package LEETCODE.BinarySearchTrees.problems;
+
+import LEETCODE.trees.BinaryTree;
+
+public class SearchInBST {
+    privat
+    public static void main(String[] args) {
+        BinaryTree.Node root = BinaryTree.createTree();
+
+    }
+}
