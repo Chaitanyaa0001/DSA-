1d36d378050ba4dabc3ca11d77bfc0858bd69436
Chaitanyaa0001 <chaitanyakhurana0@gmail.com>
2026-07-21 23:12:07 +0530

    p

---
+++ LEETCODE/BinarySearchTrees/problems/constructBSTfromPreorder.java
@@ -0,0 +1,22 @@
+package LEETCODE.BinarySearchTrees.problems;
+
+import LEETCODE.trees.BinaryTree;
+
+public class constructBSTfromPreorder {
+    private static void insert(BinaryTree.Node root, int val){
+        // BinaryTree.Node  node = new BinaryTree.Node(val);
+       
+    }
+    // private static BinaryTree.Node construct(int[] preorder){
+    //     for(int num : preorder){
+
+    //     }
+    //     re
+    // }
+    public static void main(String[] args) {
+        BinaryTree.Node root = BinaryTree.createTree();
+        // int a[] = {8,5,1,7,10,12};
+        insert(root, 0);
+
+    }
+}
