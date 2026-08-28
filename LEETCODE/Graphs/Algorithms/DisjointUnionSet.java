5d67de40fc71249b604394e4dd465177545f048a
Chaitanyaa0001 <chaitanyakhurana0@gmail.com>
2026-08-29 00:01:06 +0530

    p

---
+++ LEETCODE/Graphs/Algorithms/DisjointUnionSet.java
@@ -0,0 +1,20 @@
+package LEETCODE.Graphs.Algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+/**
+ * DisjointUnionSet
+ */
+public class DisjointUnionSet {
+    public class DisJointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
